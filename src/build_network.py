90effdc383e678adf648e257b2cddc638f06497c
Ben Heil <ben.jer.heil@gmail.com>
2022-03-29 12:46:08 -0400

    Add a script to create networkx graphs from coci citations + mesh metadata

---
+++ src/build_network.py
@@ -0,0 +1,71 @@
+import argparse
+import glob
+import pickle
+
+import networkx as nx
+import pandas as pd
+from pubmedpy.efetch import extract_all
+from pubmedpy.xml import iter_extract_elems
+from tqdm import tqdm
+
+
+
+
+# TODO load citation metadata for filtering
+# TODO add graph saving
+
+def parse_metadata(file_path: str) -> pd.DataFrame:
+    '''
+    Convert the file containing information about a set of articles into a dataframe
+
+    Parameters
+    ----------
+    file_path: The path to the file to load
+
+    Returns
+    -------
+    article_df: The dataframe of information about the articles
+
+    Note:
+    This function is based on code from Le et al., and used in accordance with their license:
+    https://github.com/greenelab/iscb-diversity/blob/master/02.process-pubmed.ipynb
+    '''
+    articles = []
+    # generator of XML PubmedArticle elements
+    article_elems = iter_extract_elems(file_path, tag='PubmedArticle')
+
+    for elem in article_elems:
+        # Example efetch XML for <PubmedArticle> at https://github.com/dhimmel/pubmedpy/blob/f554a06e13e24d661dc5ff93ad07179fb3d7f0af/pubmedpy/data/efetch.xml
+        articles.append(extract_all(elem))
+
+    return pd.DataFrame(articles)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir',
+                         help='The directory containing coci citations',
+                         default='data/coci')
+    parser.add_argument('metadata_file',
+                        help='File from download_article_metadata containing info on the '
+                             'articles to add to the network')
+    parser.add_argument('out_file',
+                        help='The file to save the resulting netowrk to')
+    args = parser.parse_args()
+
+    print('Parsing metadata...')
+    article_df = parse_metadata(args.metadata_file)
+
+    print('Building graph...')
+    # For reasons that are unclear to me, this is faster than either inserting all
+    # edges at once or constructing the graph in one shot
+    graph = nx.DiGraph()
+    network_dois = set(article_df['doi'])
+    for file_path in tqdm(glob.glob(f'{args.data_dir}/*')):
+        citation_list = pd.read_csv(file_path)
+        for citing, cited in zip(citation_list['citing'], citation_list['cited']):
+            if citing in network_dois or cited in network_dois:
+                graph.add_edge(citing, cited)
+
+    with open(args.out_file, 'wb') as out_file:
+        pickle.dump(graph, out_file)
