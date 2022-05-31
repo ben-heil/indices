f35d2691eeb94c85d43b38853e7ca8753ad7ba19
Ben Heil <ben.jer.heil@gmail.com>
2022-05-31 13:36:31 -0400

    Add documentation

--- indices/algos.py
+++ indices/algos.py
@@ -2,18 +2,65 @@ from networkx import DiGraph
 
 
 def cites_downstream(in_doi: str, graph: DiGraph, out_dois: set) -> bool:
-    for in_source, in_target in graph.out_edges(in_doi):
-        if in_source == in_doi and in_target in out_dois:
+    """
+    Checks whether `in_doi` cites (has an out_edge pointing to) any papers in `out_dois`.
+
+    Arguments
+    ---------
+    in_doi: The doi to check
+    graph: The graph containing all citations
+    out_dois: The set of dois that may be cited by `in_doi`
+
+    Returns
+    -------
+    is_cited: True if `in_doi` cites something in `out_doi`, otherwise False
+    """
+    citations = 0
+    for _, in_target in graph.out_edges(in_doi):
+        if in_target in out_dois:
             return True
     return False
 
 def get_citation_count(dois: set, graph: DiGraph) -> int:
-    downstream_citations = 0
+    """
+    Get the number of citations (in_edges) for the given set of dois.
+
+    Arguments
+    ---------
+    dois: The dois whose citations should be counted
+    graph: The graph containing all citations
+
+    Returns
+    -------
+    citations: The number of articles citing the given dois
+    """
+    citations = 0
     for doi in dois:
-        downstream_citations += graph.in_degree(doi)
-    return downstream_citations
+        citations += graph.in_degree(doi)
+    return citations
 
 def disruption_index(doi: str, graph: DiGraph) ->  float:
+    """
+    Calculates the disruption index from Wu, Wang, and Evans.
+
+    In short, the disruption index is the difference between the number of the articles citing
+    `doi` but none of its children and the number of articles citing both `doi` and at least
+    one of its children.
+
+    Arguments
+    ---------
+    doi: The doi to calculate the disruption index for
+    graph: The graph containing all citations
+
+    Returns
+    -------
+    disruption - The disruption index for the given doi
+
+    References
+    ----------
+    - Wu, L., Wang, D. & Evans, J.A. Large teams develop and small teams disrupt science
+      and technology. Nature 566, 378–382 (2019). https://doi.org/10.1038/s41586-019-0941-9
+    """
     # The set of all dois cited by the node of interest
     out_dois = set()
     # Nodes citing the node of interest but not the articles it cites
@@ -30,6 +77,7 @@ def disruption_index(doi: str, graph: DiGraph) ->  float:
         else:
             center_only += 1
 
+    # TODO fix to get # of unique papers citing instead of # of citations
     downstream_citations = get_citation_count(out_dois, graph)
     downstream_citations -= graph.out_degree(doi)
 
