8b5051e470152f8aadb98ed088badab5410c33b9
Ben Heil <ben.jer.heil@gmail.com>
2022-09-28 13:11:59 -0400

    Add script to filter dataframes to include only high-overlap fields

---
+++ indices/get_high_overlap_fields.py
@@ -0,0 +1,28 @@
+import argparse
+import glob
+import os
+import pickle as pkl
+import shutil
+
+OVERLAP_THRESHOLD = 1000
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('in_dir', help='The pagerank results to be filtered')
+    parser.add_argument('out_dir', help='The location to store results with high overlap')
+    args = parser.parse_args()
+
+    i = 0
+    for path in glob.glob(os.path.join(args.in_dir, '*.pkl')):
+        filename = os.path.basename(path)
+        try:
+            with open(path, 'rb') as in_file:
+                df = pkl.load(in_file)
+        except FileNotFoundError:
+            continue
+
+        if len(df) > OVERLAP_THRESHOLD:
+            i += 1
+            shutil.copyfile(path, os.path.join(args.out_dir, filename))
+
+    print(f'{i} heading pairs have > {OVERLAP_THRESHOLD} shared papers')
